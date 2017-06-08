267b659bf8be0fc5abb102d01da4e43bf1c7a902
TOMOAKI HASHIBA <tomoaki.hashiba@live.jp>
2017-06-09 05:33:31 +0900

    checking

--- CryptoAccouting/CoreClass/CryptoAccoutingCore.cs
+++ CryptoAccouting/CoreClass/CryptoAccoutingCore.cs
@@ -1,25 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace CryptoAccouting
-{
-    public class CryptoAccoutingCore
-    {
-
-        private List<AssetAttribute> assets;
-        private List<Position> positions;
-
-        public CryptoAccoutingCore()
-        {
-            assets = new List<AssetAttribute>();
-            positions = new List<Position>();
-
-        }
-
-        private void fetchAssets()
-        {
-
- 
-        }
-    }
-}
+﻿using System; using System.Collections.Generic;  namespace CryptoAccouting {     public class CryptoAccoutingCore     {          private List<AssetAttribute> assets;         private List<Position> positions;          public CryptoAccoutingCore()         {             assets = new List<AssetAttribute>();             positions = new List<Position>();          }          private void fetchAssets()         {            }     } } 
