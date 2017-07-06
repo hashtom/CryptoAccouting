29cd3cf396303c90debc14e2517709178859b4a0
hashtom <okadama0104>
2017-07-06 19:22:14 +0900

    checkin

---
+++ CryptoAccouting/TransactionViewController.cs
@@ -0,0 +1,85 @@
+using Foundation;
+using System;
+using UIKit;
+using CoreGraphics;
+using Syncfusion.SfDataGrid;
+
+namespace CryptoAccouting
+{
+    public partial class TransactionViewController : UIViewController
+    {
+		SfDataGrid sfGrid;
+
+        public TransactionViewController(IntPtr handle) : base(handle)
+        {
+			sfGrid = new SfDataGrid();
+			sfGrid.HeaderRowHeight = 45;
+			sfGrid.RowHeight = 45;
+
+			//sfGrid.AutoGeneratingColumn += HandleAutoGeneratingColumn;
+            sfGrid.AllowSorting = true;
+            sfGrid.AutoGenerateColumns = false;
+			//sfGrid.GroupColumnDescriptions.Add(new GroupColumnDescription() { ColumnName = "txid" });
+
+			GridTextColumn txIdColumn = new GridTextColumn();
+			txIdColumn.MappingName = "txid";
+			txIdColumn.HeaderText = "txID";
+
+			GridTextColumn coinColumn = new GridTextColumn();
+			coinColumn.MappingName = "CoinName";
+			coinColumn.HeaderText = "Coin";
+
+			GridTextColumn buysellColumn = new GridTextColumn();
+			buysellColumn.MappingName = "BuySell";
+            buysellColumn.HeaderText = "BuySell";
+
+			GridTextColumn amountColumn = new GridTextColumn();
+			amountColumn.MappingName = "Amount";
+            amountColumn.HeaderText = "Amount";
+
+			GridTextColumn priceColumn = new GridTextColumn();
+			priceColumn.MappingName = "TradePrice";
+			priceColumn.HeaderText = "Price";
+
+			GridTextColumn valueColumn = new GridTextColumn();
+			valueColumn.MappingName = "TradeValue";
+            valueColumn.HeaderText = "Value";
+
+			GridTextColumn dateColumn = new GridTextColumn();
+			dateColumn.MappingName = "TradeDate";
+            dateColumn.HeaderText = "TradeDate";
+
+
+			sfGrid.Columns.Add(txIdColumn);
+			sfGrid.Columns.Add(coinColumn);
+            sfGrid.Columns.Add(buysellColumn);
+			sfGrid.Columns.Add(amountColumn);
+			sfGrid.Columns.Add(priceColumn);
+            sfGrid.Columns.Add(valueColumn);
+            sfGrid.Columns.Add(dateColumn);
+		}
+        
+        public async override void ViewDidLoad()
+		{
+			var exg = new ExchangeAPI();
+			var myTxs = await exg.FetchTransaction((EnuExchangeType.Zaif));
+
+            base.ViewDidLoad();			
+
+            sfGrid.ItemsSource = (myTxs.TransactionCollection);
+            this.sfGrid.Frame = new CGRect(0, 30, View.Frame.Width, View.Frame.Height);
+            sfGrid.HeaderRowHeight = 10;
+			View.AddSubview(sfGrid);
+		}
+
+		//void HandleAutoGeneratingColumn(object sender, AutoGeneratingColumnArgs e)
+		//{
+		//	if (e.Column.MappingName == "txid")
+		//	{
+		//		e.Column.TextMargin = 1;
+		//		e.Column.TextAlignment = UITextAlignment.Left;
+		//	}
+		//}
+
+    }
+}
