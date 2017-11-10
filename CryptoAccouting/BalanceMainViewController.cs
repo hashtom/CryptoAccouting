7902b5ce26f94fa7a3ed48cb4a5d9a192002a43a
hashtom <okadama0104>
2017-11-10 16:56:51 +0900

    checkin

--- CryptoAccouting/BalanceMainViewController.cs
+++ CryptoAccouting/BalanceMainViewController.cs
@@ -21,10 +21,22 @@
                     Task.Run(async () => await AppCore.FetchCoinLogoTop100Async());                 }              }             catch (Exception e)             {
                 this.PopUpWarning("Error", "Critical issue: " + e.GetType() + ": " + e.Message);                 this.mybalance = new Balance();             } 
         }          public async override void ViewWillAppear(bool animated)         {
-            base.ViewWillAppear(animated);              if (position_count != mybalance.BalanceByCoin.Count)             {                 TableView.Source = new CoinTableSource(mybalance, this);                 position_count = mybalance.BalanceByCoin.Count;             }              ReDrawScreen(); //need this              try             {
-                await AppCore.LoadCrossRateAsync();
-                await AppCore.FetchMarketDataFromBalanceAsync();                 await AppCore.FetchCoinLogoFromBalanceAsync();
-            }             catch(Exception e)             {                 //this.PopUpWarning("Unable to obtain latest prices: " + e.GetType() + ": " + e.Message);                 System.Diagnostics.Debug.WriteLine(DateTime.Now.ToString() + ": ViewWillAppear: Cound't update price: " + e.GetType() + ": " + e.Message);             }             finally             {                 ReDrawScreen();             }
+            base.ViewWillAppear(animated);              if (position_count != mybalance.BalanceByCoin.Count)             {                 TableView.Source = new CoinTableSource(mybalance, this);                 position_count = mybalance.BalanceByCoin.Count;             }              ReDrawScreen(); //need this              var updatetime = AppCore.Balance.PriceDateTime.AddMinutes(1);             if (DateTime.Now > updatetime)             {
+                try
+                {
+                    await AppCore.LoadCrossRateAsync();
+                    await AppCore.FetchMarketDataFromBalanceAsync();
+                    await AppCore.FetchCoinLogoFromBalanceAsync();
+                }
+                catch (Exception e)
+                {
+                    //this.PopUpWarning("Unable to obtain latest prices: " + e.GetType() + ": " + e.Message);
+                    System.Diagnostics.Debug.WriteLine(DateTime.Now.ToString() + ": ViewWillAppear: Cound't update price: " + e.GetType() + ": " + e.Message);
+                }
+                finally
+                {
+                    ReDrawScreen();
+                }             }
 
         }          public override void PrepareForSegue(UIStoryboardSegue segue, NSObject sender)
         {
